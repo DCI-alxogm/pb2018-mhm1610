80de7589441075fbca0c11d5e98a33755705c68b
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-08-08 09:25:41 -0500

    Cree mi primer programa.

---
+++ semana1/programa-hola.c
@@ -0,0 +1,18 @@
+/*
+     Esto es un comentario. Me sirve para escribir información relevante del pro
+grama que estoy haciendo. Por  ejemplo:
+     Este programa imprime la típica frase “hola mundo a la pantalla”
+     Creado el 06/08/2018 por Mariana Herrea Mosqueda…
+    */
+/*Creado el 08 de agosto de 2018 por Mariana Herrera Mosqueda*/
+#include<stdio.h>
+
+int main()
+{
+
+ printf("Hola Mariana\n"); 
+ printf("Este es el primer programa del curso\n"); 
+
+}
+
+/*Termine de escribir el programa*/
