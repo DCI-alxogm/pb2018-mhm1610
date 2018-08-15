a2036ead61551d24b06574627717cc915c9642d8
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-08-15 10:06:30 -0500

    Actualizacion semana 2

---
+++ semana2/programa2.c
@@ -0,0 +1,20 @@
+/*Creado el 15 de Agosto de 2018 por Mariana Herrera*/
+#include<stdio.h>
+
+int edad,m,act,i=2028,o;
+
+int main()
+{
+printf("Hola Mariana \n");
+printf("Este es el primer programa del curso");
+printf("Introduce tu edad: \n");
+scanf("%d",&edad);
+printf("Introduce el año en el que estas: \n");
+scanf("%d",&m);
+o=i-m;
+act=edad+o;
+printf("\nTu edad en 2028 es:%d\n",act);
+return 0;
+}
+ 
+ 
