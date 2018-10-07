1e453abe38f961926a37d12dc03559e948bbd631
mhm1610 <42146384+mhm1610@users.noreply.github.com>
2018-10-06 19:24:39 -0500

    Ejemplos
    Ejemplos

---
+++ semana8/ejemplo4.c
@@ -0,0 +1,27 @@
+//Ejemplo 4//
+
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+
+int num, i, *ptr, sum=0;
+
+printf("Introduce el numero de elementos: ");
+scanf("%d", &num);
+
+ptr= (int*) calloc(num, sizeof(int));
+if(ptr==NULL)
+{
+	printf("Error! memoria no reservada");
+	exit(0);
+}
+printf("Introduce los elementos de el arreglo: ");
+for(i=0; i<num; ++i)
+{
+	scanf("%d", ptr+i);
+	sum+=*(ptr+i);
+}
+printf("Sum= %d", sum);
+free(ptr);
+return 0;
+}
