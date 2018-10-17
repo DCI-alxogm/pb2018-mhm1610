c1e6f9c37f8b32e13ac230b4c397056fedcd1056
Herrera Mosqueda Mariana <herreramm2018@dci25-178.ugto.mx>
2018-10-17 09:40:25 -0500

    Ejercicio donde juntos dos archivos en uno para poder crear y funcionar mi programa que es de sacar la mita de un numero

---
+++ semana10/ejercicio.c
@@ -0,0 +1,11 @@
+//Primera parte//
+//17 de octubre de 2018//
+#include<stdio.h>
+void mitad(int x);
+int main(){
+	int a;
+	printf("Introduce un nnumero para saber su mitad: \n");
+	scanf("%f", &a);
+	mitad(a);
+	return 0;
+}
---
+++ semana10/ejercicio1.c
@@ -0,0 +1,8 @@
+//segunda parte//
+#include<stdio.h>
+void mitad(float x){
+float b;
+b=x/2;
+printf("La mitad de %f es %f\n", x,b);
+}
+
